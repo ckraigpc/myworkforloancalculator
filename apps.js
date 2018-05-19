91c865eae971135bd1758279c75418e9a56f6e19
ckraig <ckraigpc@gmail.com>
2018-05-19 08:14:46 -0700

    Check the instruction on you script here

---
+++ apps.js
@@ -0,0 +1,64 @@
+// listen for submit
+
+
+  // hide results
+
+
+// show progressbar
+
+
+
+// calculate results
+
+  // get Loan Calculator heading UI variables
+ 
+
+  // get Results heading UI variables
+
+
+  /** parse or convert in a number type the value of "amount,interest,yearstopay"
+   *  interest is calculate as divide 100 and divide by 12 
+   * years to pay for payment is multiplied by 12
+  **/
+ 
+
+  /** compute monthy payment
+   *  use Math.pow -- google on how to use this 
+   */
+
+  // validate if the monthly payment is finite use isFinite predefined method in javascript
+ 
+    // set the value of montlypayment,totalpayment to monthly result with a decimal places of two
+    
+
+    // display the block of results
+ 
+
+    // hide the progress bar
+   
+
+    // call a function that showError , make sure to create the function
+
+
+// show error function
+
+  // hide progress bar 
+ 
+  // create an element div
+ 
+
+  // use card and span heading to display the message
+
+
+  // add class on the div select on masterializecss
+
+  // create a textcontent on it
+
+
+  // insert error above heading
+
+
+  // clear error after 3 seconds using setTimeout and execute remove method on the function clear error
+
+
+  // clear error function
